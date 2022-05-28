7cdce072762642a9a7b6f94b7f7c1a002bc4959a
Adam Ralph <adamralph@posteo.net>
2022-05-28 12:04:56 +1000

    updates

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
 
-app = express()
+app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('static'))
-app.use(express.urlencoded({ extended: true }))
-app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')))
+app.use(express.static('static'));
+app.use(express.urlencoded({ extended: true }));
+app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
 // Articles display
 app.get('/articles', (req, res) => {
-  res.render('articles')
-})
+  res.render('articles');
+});
 
 // Individual article
 app.get('/read', (req, res) => {
-  res.render('read')
-})
+  res.render('read');
+});
 
 app.get('/about', (req, res) => {
-  res.render('about')
-})
-app.listen(3001, () => {
-  console.log('Listening on port 3001')
-})
+  res.render('about');
+});
+
+const port = 3001;
+app.listen(`${port}`, () => {
+  console.log(`Server running on port ${port} : http://localhost:${port}`);
+});
