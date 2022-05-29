b66a1820a2a5596a2abfacc54517b863ebdde703
Adam Ralph <adamralph@posteo.net>
2022-05-29 12:41:37 +1000

    database connected

---
+++ db.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb')
+
+require('dotenv').config({ path:__dirname + '/.env' })
+const TOKEN = process.env.DB_KEY
+
+async function main () {
+
+  const uri = `mongodb+srv://dbadmin:${TOKEN}@cluster0.u8vh2.mongodb.net/?retryWrites=true&w=majority`
+
+  const client = new MongoClient(uri)
+  try {
+    await client.connect()
+    console.log('Connection open...')
+
+    // await createPost(client, {
+    //   title: 'Bockchain Decrypted',
+    //   summary: "This website is for exploring the tech behind blockchain and it's impact on the world at large",
+    //   author: 'The Chain Link',
+    //   published: false,
+    //   content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magnam esse harum magni, totam numquam quaerat nemo necessitatibus animi expedita!"
+    // })
+
+  } catch (e) {
+    console.log('Unable to connect...')
+    console.error(e)
+  } finally {
+    await client.close()
+  }
+}
+
+main().catch(console.error)
+
+async function createPost(client, newPost) {
+  const result = await client.db('chainlink').collection('posts').insertOne(newPost)
+
+  console.log(`New post created with the following id: ${result.insertedId}`)
+}
+
